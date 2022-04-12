b1dcee4e0e8a0ae5f45ea7104797b775e4d9b060
stlad <82213506+stlad@users.noreply.github.com>
2022-04-12 15:25:59 +0500

    формирование популяции

--- model.py
+++ model.py
@@ -12,21 +12,30 @@ class Model:
         self.current_population = []
         self.grey_nums = ml.get_grey_codes(grid_count)
         self.interval = (interval[0], interval[1])
+        self.grid_step = (interval[1] - interval[0])/(2**grid_count)
+
 
     def get_start_population(self):
         #self.current_population = [Individ(i,self.function(i)) for i in range(self.population_count)] # предварительно
         for i in range(self.population_count):
-            ind_x = random.randint(0, 2**self.grid_count)
-            ind_y = self.function(ind_x)
+            x = random.randint(0,2**self.grid_count-1)
+            global_x = ml.grid_index_to_global(self, x)
+
+            ind_x = self.grey_nums[x]
+            ind_y = self.function(global_x)
 
             self.current_population.append(Individ(ind_x, ind_y))
 
 
 
 
+
+
+
+
 class Individ:
-    def __init__(self, x, y):
-        self.x = x
+    def __init__(self, gene, y):
+        self.gene = gene
         self.y = y
 
 
