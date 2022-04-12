546970ac05d44b66d296445455e4aa830c3dd5e7
stlad <82213506+stlad@users.noreply.github.com>
2022-04-12 15:25:48 +0500

    работа с отображением (предварительно)

--- main.py
+++ main.py
@@ -1,19 +1,28 @@
 import model as md
+import math_lib as ml
 import matplotlib.pyplot as plt
+import math
 
 def func(x):
     return x**2
 
 
-m = md.Model(5, 5, func, [0,5])
-
-x = []
+m = md.Model(10, 5, func, [-32,32])
+x=[]
 y=[]
 m.get_start_population()
 for i in m.current_population:
-    x.append(i.x)
     y.append(i.y)
+    x_index =ml.grey_to_num(i.gene)
+    x_global = ml.grid_index_to_global(m, x_index)
+    x.append(x_global)
+
+
+func_x = [a for a in range(0,32)]
+func_y = [func(a) for a in func_x]
 
 
 plt.scatter(x,y)
+plt.plot(func_x, func_y)
+plt.grid()
 plt.show()
