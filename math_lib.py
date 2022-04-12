6d1e1e38f1cf12bc3e08101b243916b09cdf1acc
stlad <82213506+stlad@users.noreply.github.com>
2022-04-12 15:25:33 +0500

    перевод гена в координаты

--- math_lib.py
+++ math_lib.py
@@ -4,6 +4,7 @@
 def num_to_grey_code(num):
     return num ^ (num >> 1)
 
+
 def get_grey_codes(n, res =[]):
     res = []
     for i in range(2**n):
@@ -12,4 +13,15 @@ def get_grey_codes(n, res =[]):
 
 
 
+def grey_to_num(gr):
+    bin =0
+    while gr>0:
+        bin = bin^gr
+        gr =gr >>1
+    return bin
 
+
+def grid_index_to_global(model, index):
+    a = model.interval[0]
+    global_pos = a + (model.grid_step * index + (model.grid_step / 2))
+    return global_pos
