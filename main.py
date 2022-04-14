44bd7b8c3ffa0ee87af8384632650c09e102f563
stlad <82213506+stlad@users.noreply.github.com>
2022-04-14 17:40:38 +0500

    upd main

--- main.py
+++ main.py
@@ -3,44 +3,81 @@ import model as md
 import math_lib as ml
 import matplotlib.pyplot as plt
 import math
+import functions
 
-def func(x):
-    return x**5+6*x**4+3*x**3+x**2-10*x+5
+def create_model():
+    print('Введите параметры модели:')
+    print('Размер популяции = ',end = '')
+    pop_count = int(input())
+    print('количество узлов сетки = 2^', end = '')
+    gr_count = int(input())
+    print('номер функции = ', end = '')
+    func_num = int(input())
+    func = functions.func_dict[func_num]
 
+    model = md.Model(pop_count, gr_count, func[0], func[1])
 
-m = md.Model(10, 6, func, [-5,5])
-m.get_start_population()
+    print('Изменить доп. параметры? [y/n] ', end='')
+    if input()=='y':
+        print(f'Вероятность скрещивания (по умолчанию {model.PR_CROSSOVER}) = ', end = '')
+        model.PR_CROSSOVER = float(input())
+        print(f'Вероятность мутации (по умолчанию {model.PR_MUTATION}) = ', end = '')
+        model.PR_MUTATION = float(input())
 
-for gen in range(10):
+    print(f'Изменить интервал (по умолчанию {model.interval}) ? [y/n] ', end='')
+    if input() == 'y':
+        print('Начало = ', end = '')
+        a = float(input())
+        print('Конец = ', end = '')
+        b = float(input())
+        model.interval = (a,b)
 
-    x=[]
-    y=[]
+    return model
 
-    for i in m.current_population:
-        y.append(i.fitness)
-        x_index =ml.grey_to_num(i.gene)
-        x_global = ml.grid_index_to_global(m, x_index)
-        x.append(x_global)
+def draw_graph(model):
+    dots_x = []
+    dots_y = []
 
+    for ind in m.current_population:
+        dots_y.append(ind.fitness)
+        dots_x.append(ml.gene_to_global_coords(model, ind.gene))
 
-    func_x = [a for a in range(-5,5)]
-    func_y = [func(a) for a in func_x]
+    plt.scatter(dots_x, dots_y, edgecolors='red')
 
-    plt.scatter(x,y)
-    plt.plot(func_x, func_y)
+    x = []
+    for index in range(-2,2**model.grid_count+2):
+        x.append(model.interval[0] + index*model.grid_step)
+    y = [model.function(fx) for fx in x]
+
+    plt.plot(x,y)
     plt.grid()
+    plt.title(f'Поколение № {model.generation_number}')
     plt.show()
 
-    print(f'номер поколения:{m.generation_number}')
-    for ind in m.current_population:
-        print(f'ген: {ind.gene}| индекс: {ml.grey_to_num(ind.gene)}| глобальная X: {ml.gene_to_global_coords(m,ind.gene)}| фенотип: {ind.fitness}')
 
+
+def print_model_generaiton(model):
+    print('-----------------------------')
+    print(f'номер поколения:{m.generation_number}')
+    for ind in model.current_population:
+        print(f'ген: {ind.gene}| индекс: {ml.grey_to_num(ind.gene)}| глобальная X: {ml.gene_to_global_coords(m, ind.gene)}| фенотип: {ind.fitness}')
+
+    print('-----------------------------')
+
+
+
+m = create_model()
+m.get_start_population()
+draw_graph(m)
+print_model_generaiton(m)
+
+for i in range(5):
     m.next()
+    draw_graph(m)
+    print_model_generaiton(m)
+
 
 
 
 
 print('------------')
-'''ind = (model.get_best_fit_individ(m.current_population))
-print('-----------------')
-print(f'ген: {ind.gene}| индекс: {ml.grey_to_num(ind.gene)}| глобальная X: {ml.gene_to_global_coords(m,ind.gene)}| фенотип: {ind.fitness}')'''
